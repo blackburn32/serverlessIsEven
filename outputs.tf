57eeb99fa746c398cc1a6a639ac0ffcd5ce660a7
blackburn32 <alexblackburn32@gmail.com>
2022-05-22 17:34:56 -0700

    Clean up outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
-output "lambda_bucket_name" {
-  description = "Name of the S3 bucket used to store function code."
-
-  value = aws_s3_bucket.lambda_bucket.id
-}
-
-output "lambda_function_url" {
+output "baseURL" {
     description = "The URL used to invoke the lambda."
 
     value = aws_lambda_function_url.live.function_url
