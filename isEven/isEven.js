5592604f365009e31d553697353ed547a21a29b7
Alex Blackburn <ablackburn@ablackburn.dev.lucidvpc.com>
2022-06-19 11:58:50 -0700

    Get a random ad out of dynamodb and include it in the response of successful requests

--- isEven/isEven.js
+++ isEven/isEven.js
@@ -1,16 +1,17 @@
-module.exports.handler = async (event) => {
-  return handleEvent(event);
-};
-module.exports.isEven = isEven;
+const AWS = require("aws-sdk");
+const globalDynamo = new AWS.DynamoDB();
 
-function handleEvent(event) {
+const advertisementsTableName = "advertisements";
+
+async function handleEvent(event, dynamo) {
   const query = getQueryFromEvent(event);
   if (isNumeric(query)) {
     const queryAsNumber = BigInt(query);
 
     const answer = isEven(queryAsNumber);
+    const ad = await getAdFromDynamo(dynamo);
 
-    return successResponse(query, answer);
+    return successResponse(query, answer, ad);
   } else {
     logInvalidEvent(event);
     return errorResponse(query);
@@ -37,6 +38,27 @@ function logInvalidEvent(event) {
   console.log("Invalid event: ", event);
 }
 
+async function getAdFromDynamo(dynamo) {
+  const numberOfAdsRaw = await dynamo
+    .scan({
+      TableName: advertisementsTableName,
+      Select: "COUNT",
+    })
+    .promise();
+  const numberOfAds = Math.max(numberOfAdsRaw.Count, 1);
+  const luckyAdId = Math.floor(Math.random() * numberOfAds);
+
+  const adRaw = await dynamo
+    .getItem({
+      TableName: advertisementsTableName,
+      Key: {
+        id: { N: `${luckyAdId}` },
+      },
+    })
+    .promise();
+  return adRaw.Item.ad.S;
+}
+
 function errorResponse(query) {
   return {
     statusCode: 400,
@@ -44,7 +66,7 @@ function errorResponse(query) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      message: `Invalid query`,
+      message: "Invalid query",
       query: query,
     }),
   };
@@ -54,7 +76,7 @@ function isEven(query) {
   return query % 2n == 0n;
 }
 
-function successResponse(query, isEven) {
+function successResponse(query, isEven, ad) {
   return {
     statusCode: 200,
     headers: {
@@ -63,6 +85,17 @@ function successResponse(query, isEven) {
     body: JSON.stringify({
       isEven: `${isEven}`,
       query: query,
+      ad: ad,
     }),
   };
 }
+
+
+module.exports.handlerWithNewDynamo = async (event) =>{
+  const dynamo = new AWS.DynamoDB();
+  return handleEvent(event, dynamo);
+}
+module.exports.handler = async (event) => {
+  return handleEvent(event, globalDynamo);
+};
+module.exports.isEven = isEven;
--- isEven/isEven.spec.js
+++ isEven/isEven.spec.js
@@ -1,5 +1,16 @@
 const { expect } = require("expect");
-const { isEven, handler } = require("./isEven");
+const { isEven, handler, handlerWithNewDynamo } = require("./isEven");
+const AWS = require("aws-sdk-mock");
+
+AWS.mock("DynamoDB", "scan", function (params, callback) {
+  callback(null, { Count: 1 });
+});
+
+AWS.mock("DynamoDB", "getItem", function (params, callback) {
+  callback(null, {
+    Item: { id: { N: "0" }, ad: { S: "You are a beautiful person!" } },
+  });
+});
 
 test("isEven works for small numbers", () => {
   expect(isEven(2n)).toBe(true);
@@ -32,24 +43,26 @@ test("isEven works for large numbers", () => {
 });
 
 test("handler succeeds on good input", async () => {
-  const getResponse = await handler({
+  const getResponse = await handlerWithNewDynamo({
     rawPath: "/123",
   });
-  const postResponse = await handler({
+  const postResponse = await handlerWithNewDynamo({
     rawPath: "/",
     body: 2,
   });
   expect(getResponse.statusCode == 200);
   expect(getResponse.isEven == false);
+  expect(getResponse.ad == "You are a beautiful person!");
   expect(postResponse.statusCode == 200);
   expect(postResponse.isEven == true);
+  expect(postResponse.ad == "You are a beautiful person!");
 });
 
 test("handler errors on bad input", async () => {
-  const getResponse = await handler({
+  const getResponse = await handlerWithNewDynamo({
     rawPath: "/123abc",
   });
-  const postResponse = await handler({
+  const postResponse = await handlerWithNewDynamo({
     rawPath: "/",
     body: "abc",
   });
